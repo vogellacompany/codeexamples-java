b4c08ec27bd10e9116623b6c65fc5a6281881b30
Lars Vogel <Lars.Vogel@gmail.com>
2010-09-01 23:32:21 +0200

    Add a .gitignore file

--- de.vogella.android.widget.image/src/de/vogella/android/widget/image/MyWidgetProvider.java
+++ de.vogella.android.widget.image/src/de/vogella/android/widget/image/MyWidgetProvider.java
@@ -1,7 +1,21 @@
 package de.vogella.android.widget.image;
 
+import java.util.Random;
+
+import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.Context;
 
 public class MyWidgetProvider extends AppWidgetProvider  {
 
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		for (int i : appWidgetIds) {
+			int number = (new Random().nextInt());
+		}
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+	
 }
