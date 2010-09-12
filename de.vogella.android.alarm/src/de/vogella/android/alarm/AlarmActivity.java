ad194ae6bbd43776265e496f92a0970bec998581
Lars Vogel <Lars.Vogel@gmail.com>
2010-09-12 23:41:05 +0200

    Enhanced android timer project

--- de.vogella.android.alarm/src/de/vogella/android/alarm/AlarmActivity.java
+++ de.vogella.android.alarm/src/de/vogella/android/alarm/AlarmActivity.java
@@ -5,11 +5,11 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class AlarmActivity extends Activity {
-	private NumberPicker secondsPicker;
 	private NumberPicker minutesPicker;
 	private NumberPicker hoursPicker;
 
@@ -19,8 +19,6 @@ public class AlarmActivity extends Activity {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		secondsPicker = (NumberPicker) findViewById(R.id.seconds);
-		secondsPicker.setRange(1, 60);
 		minutesPicker = (NumberPicker) findViewById(R.id.minutes);
 		minutesPicker.setRange(0, 60);
 		hoursPicker = (NumberPicker) findViewById(R.id.hours);
@@ -28,14 +26,18 @@ public class AlarmActivity extends Activity {
 	}
 
 	public void startAlert(View view) {
-		int i = secondsPicker.getCurrent();
+		int minutes = minutesPicker.getCurrent()*60;
+		Log.e("Hello", String.valueOf(minutes));
+		int hours = hoursPicker.getCurrent()*60*60;
+		Log.e("Hello", String.valueOf(hours));
 		Intent intent = new Intent(this, MyBroadcastReceiver.class);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(
 				this.getApplicationContext(), 234324243, intent, 0);
 		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()
-				+ (i * 1000), pendingIntent);
-		Toast.makeText(this, "Alarm set in " + i + " seconds",
+				+ ((minutes+hours) * 1000), pendingIntent);
+		int sum = minutes +hours; 
+		Toast.makeText(this, "Alarm set in "+ sum  + " seconds",
 				Toast.LENGTH_LONG).show();
 	}
 
