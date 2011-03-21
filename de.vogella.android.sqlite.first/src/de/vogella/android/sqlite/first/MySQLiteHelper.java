5dea0f1db2259ec9946b3bc9e93961ec310a4361
Lars Vogel <Lars.Vogel@gmail.com>
2011-03-21 23:56:15 +0100

    updated training

---
+++ de.vogella.android.sqlite.first/src/de/vogella/android/sqlite/first/MySQLiteHelper.java
@@ -0,0 +1,29 @@
+package de.vogella.android.sqlite.first;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME = "applicationdata";
+	private static final int DATABASE_VERSION = 1;
+	// Database creation sql statement
+	private static final String DATABASE_CREATE = "create table comments (_id integer primary key autoincrement, "
+			+ "comment text not null);";
+
+	public MySQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Not implemented
+	}
+
+}
---
+++ de.vogella.android.sqlite.first/src/de/vogella/android/sqlite/first/TestDatabase.java
@@ -0,0 +1,41 @@
+package de.vogella.android.sqlite.first;
+
+import java.util.Random;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SimpleCursorAdapter;
+
+public class TestDatabase extends ListActivity {
+    private CommentsDbAdapter db;
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // From this column of the table
+        String[] from = new String[] { CommentsDbAdapter.KEY_COMMENT};
+		// To which view element will this be mapped
+        int[] to = new int[] { android.R.id.text1 };
+        db = new CommentsDbAdapter(this);
+        db.open();
+        
+        // Create a new comment every time the activity is started
+        
+        String[] comments = new String[] {"Cool", "Very nice", "Hate it"};
+    	int nextInt = new Random().nextInt(3);
+    	db.createComment(comments[nextInt]);
+        
+        Cursor c = db.fetchAllComments();
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, c, from, to);
+        setListAdapter(adapter);
+    }
+    @Override
+    protected void onPause() {
+    	db.close();
+    	super.onPause();
+    }
+}
