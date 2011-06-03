37f692e57ed44c041a3944a9207aa6bc331da547
Lars Vogel <Lars.Vogel@gmail.com>
2011-06-04 00:05:15 +0200

    new examples

---
+++ de.vogella.android.kidsdrawing/src/de/vogella/android/kidsdrawing/FingerPaintActivity.java
@@ -0,0 +1,37 @@
+package de.vogella.android.kidsdrawing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class FingerPaintActivity extends Activity {
+	private DrawingView view;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// hide statusbar of Android
+		// could also be done later
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		view = new DrawingView(this, null);
+		setContentView(view);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		view.clear();
+		return super.onOptionsItemSelected(item);
+	}
+}
