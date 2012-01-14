ce28f078b4d20c7f9e7090b1fed2aedd15e2df71
Lars Vogel <Lars.Vogel@gmail.com>
2012-01-14 12:30:45 +0100

    updates

--- de.vogella.concurrency.nonblocking.counter/src/de/vogella/concurrency/nonblocking/counter/Counter.java
+++ de.vogella.concurrency.nonblocking.counter/src/de/vogella/concurrency/nonblocking/counter/Counter.java
@@ -3,22 +3,24 @@ package de.vogella.concurrency.nonblocking.counter;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Counter {
-	private AtomicInteger value = new AtomicInteger(); 
-	public int getValue(){
+	private AtomicInteger value = new AtomicInteger();
+
+	public int getValue() {
 		return value.get();
 	}
-	public int increment(){
+
+	public int increment() {
 		return value.incrementAndGet();
 	}
-	
-	// Alternative implementation as increment but just make the 
+
+	// Alternative implementation as increment but just make the
 	// implementation explicit
-	public int incrementLongVersion(){
+	public int incrementLongVersion() {
 		int oldValue = value.get();
-		while (value.compareAndSet(value.get(),oldValue+1)){
-			 oldValue = value.get();
+		while (!value.compareAndSet(value.get(), oldValue + 1)) {
+			oldValue = value.get();
 		}
-		return oldValue+1;
+		return oldValue + 1;
 	}
-	
+
 }
