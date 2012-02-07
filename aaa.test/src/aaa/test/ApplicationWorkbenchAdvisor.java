205f9dd5b15918e82502c9aba84418eebb2ea028
Lars Vogel <Lars.Vogel@gmail.com>
2012-02-07 08:58:45 +0100

    updates

---
+++ aaa.test/src/aaa/test/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,18 @@
+package aaa.test;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+
+	private static final String PERSPECTIVE_ID = "aaa.test.perspective"; //$NON-NLS-1$
+
+    public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+        return new ApplicationWorkbenchWindowAdvisor(configurer);
+    }
+
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+}
---
+++ aaa.test/src/aaa/test/Perspective.java
@@ -0,0 +1,11 @@
+package aaa.test;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+	}
+}
