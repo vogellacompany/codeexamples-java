ba5ca312d8105394a971c3d61796454424b51dc5
Lars Vogel <lars.vogel@gmail.com>
2012-06-06 01:48:02 +0200

    updates

--- de.vogella.java.io/src/de/vogella/java/io/MyFile.java
+++ de.vogella.java.io/src/de/vogella/java/io/MyFile.java
@@ -11,7 +11,7 @@ public class MyFile {
 
 	public String readTextFile(String fileName) {
 		String returnValue = "";
-		FileReader file;
+		FileReader file = null;
 		String line = "";
 		try {
 			file = new FileReader(fileName);
@@ -23,19 +23,34 @@ public class MyFile {
 			throw new RuntimeException("File not found");
 		} catch (IOException e) {
 			throw new RuntimeException("IO Error occured");
+		} finally {
+			if (file != null) {
+				try {
+					file.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 		return returnValue;
-
 	}
 
 	public void writeTextFile(String fileName, String s) {
-		FileWriter output;
+		FileWriter output = null;
 		try {
 			output = new FileWriter(fileName);
 			BufferedWriter writer = new BufferedWriter(output);
 			writer.write(s);
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 
 	}
