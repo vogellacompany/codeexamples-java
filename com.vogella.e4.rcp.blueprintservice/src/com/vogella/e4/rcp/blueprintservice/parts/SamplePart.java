63809dbb33b331a7e1d3bb3080afc477294b37db
Lars Vogel <Lars.Vogel@gmail.com>
2012-10-10 14:00:55 +0200

    updates

---
+++ com.vogella.e4.rcp.blueprintservice/src/com/vogella/e4/rcp/blueprintservice/parts/SamplePart.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package com.vogella.e4.rcp.blueprintservice.parts;
+
+import javax.annotation.PostConstruct;
+import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+public class SamplePart {
+
+	private Label label;
+	private TableViewer tableViewer;
+
+	@PostConstruct
+	public void createComposite(Composite parent) {
+		parent.setLayout(new GridLayout());
+
+		label = new Label(parent, SWT.NONE);
+		label.setText("Sample table");
+
+		tableViewer = new TableViewer(parent);
+		tableViewer.add("Sample item 1");
+		tableViewer.add("Sample item 2");
+		tableViewer.add("Sample item 3");
+		tableViewer.add("Sample item 4");
+		tableViewer.add("Sample item 5");
+		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+	}
+
+	@Focus
+	public void setFocus() {
+		tableViewer.getTable().setFocus();
+	}
+}
