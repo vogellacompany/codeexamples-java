4de069e06a5145e5e8e665e529a2056607c71e30
Lars Vogel <Lars.Vogel@gmail.com>
2012-10-16 09:06:35 +0200

    updates

--- de.vogella.android.notificationmanager/src/de/vogella/android/notificationmanager/NotificationReceiverActivity.java
+++ de.vogella.android.notificationmanager/src/de/vogella/android/notificationmanager/NotificationReceiverActivity.java
@@ -3,7 +3,7 @@ package de.vogella.android.notificationmanager;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class NotificationReceiver extends Activity {
+public class NotificationReceiverActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
