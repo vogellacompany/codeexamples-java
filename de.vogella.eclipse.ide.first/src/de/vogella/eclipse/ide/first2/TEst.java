ce03244692de638845494670a7e763b33ab3b4a8
Lars Vogel <Lars.Vogel@gmail.com>
2012-12-28 22:19:41 +0100

    updates

---
+++ de.vogella.eclipse.ide.first/src/de/vogella/eclipse/ide/first2/TEst.java
@@ -0,0 +1,13 @@
+package de.vogella.eclipse.ide.first2;
+
+public class TEst {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
