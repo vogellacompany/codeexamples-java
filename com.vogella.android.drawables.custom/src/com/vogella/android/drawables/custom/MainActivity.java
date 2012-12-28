4f19acf5b813c1856ea1c31f7c3e81625dea7e7e
Lars Vogel <lars.vogel@gmail.com>
2012-12-28 21:00:23 +0100

    updates

---
+++ com.vogella.android.drawables.custom/src/com/vogella/android/drawables/custom/MainActivity.java
@@ -0,0 +1,23 @@
+package com.vogella.android.drawables.custom;
+
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		ImageView button = (ImageView) findViewById(R.id.image);
+		InputStream resource = getResources().openRawResource(R.drawable.dog);
+		Bitmap bitmap = BitmapFactory.decodeStream(resource);
+		button.setBackground(new MyRoundCornerDrawable(bitmap));
+	}
+
+}
