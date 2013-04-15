fc5ab334a29d58593742f8db399c7f7206394c15
Lars Vogel <Lars.Vogel@gmail.com>
2013-04-15 12:06:36 +0200

    updates

--- android.jax.live/src/android/jax/live/MainActivity.java
+++ android.jax.live/src/android/jax/live/MainActivity.java
@@ -1,20 +1,5 @@
 package android.jax.live;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends Activity {
 	private static final int PICK_PHOTO = 10;
 	private Bitmap bitmap;
