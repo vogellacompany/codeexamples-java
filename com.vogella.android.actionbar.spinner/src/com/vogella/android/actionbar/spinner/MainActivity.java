f38e006f3c1118ec1e3cb090b5be1c3008102045
Lars Vogel <lars.vogel@gmail.com>
2013-04-15 07:41:20 +0200

    updated Android Actionbar

--- com.vogella.android.actionbar.spinner/src/com/vogella/android/actionbar/spinner/MainActivity.java
+++ com.vogella.android.actionbar.spinner/src/com/vogella/android/actionbar/spinner/MainActivity.java
@@ -31,15 +31,23 @@ public class MainActivity extends FragmentActivity implements
 		actionBar.setDisplayShowTitleEnabled(false);
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-		// Set up the dropdown list navigation in the action bar.
-		actionBar.setListNavigationCallbacks(
+		final String[] dropdownValues = getResources().getStringArray(
+				R.array.dropdown);
+
 		// Specify a SpinnerAdapter to populate the dropdown list.
-				new ArrayAdapter<String>(actionBar.getThemedContext(),
-						android.R.layout.simple_list_item_1,
-						android.R.id.text1, new String[] {
-								getString(R.string.title_section1),
-								getString(R.string.title_section2),
-								getString(R.string.title_section3), }), this);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				actionBar.getThemedContext(),
+				android.R.layout.simple_spinner_item, android.R.id.text1,
+				dropdownValues);
+
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		// Set up the dropdown list navigation in the action bar.
+		actionBar.setListNavigationCallbacks(adapter, this);
+
+		// Use getActionBar().getThemedContext() to ensure
+		// that the text color is always appropriate for the action bar
+		// background rather than the activity background.
 	}
 
 	@Override
@@ -60,7 +68,6 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
@@ -79,24 +86,15 @@ public class MainActivity extends FragmentActivity implements
 	}
 
 	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
+	 * A dummy fragment
 	 */
 	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
 
-		public DummySectionFragment() {
-		}
+		public static final String ARG_SECTION_NUMBER = "placeholder_text";
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
 				Bundle savedInstanceState) {
-			// Create a new TextView and set its text to the fragment's section
-			// number argument value.
 			TextView textView = new TextView(getActivity());
 			textView.setGravity(Gravity.CENTER);
 			textView.setText(Integer.toString(getArguments().getInt(
