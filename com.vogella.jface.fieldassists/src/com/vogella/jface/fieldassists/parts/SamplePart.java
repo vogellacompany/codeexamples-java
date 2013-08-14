710a0bf6a03935fb5cd175d88327ba571a0f274c
Lars Vogel <Lars.Vogel@gmail.com>
2013-08-14 12:09:18 +0200

    New Android examples

--- com.vogella.jface.fieldassists/src/com/vogella/jface/fieldassists/parts/SamplePart.java
+++ com.vogella.jface.fieldassists/src/com/vogella/jface/fieldassists/parts/SamplePart.java
@@ -10,38 +10,92 @@
  *******************************************************************************/
 package com.vogella.jface.fieldassists.parts;
 
+
 import javax.annotation.PostConstruct;
+
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.jface.bindings.keys.KeyStroke;
+import org.eclipse.jface.bindings.keys.ParseException;
+import org.eclipse.jface.fieldassist.ContentProposalAdapter;
+import org.eclipse.jface.fieldassist.ControlDecoration;
+import org.eclipse.jface.fieldassist.FieldDecorationRegistry;
+import org.eclipse.jface.fieldassist.SimpleContentProposalProvider;
+import org.eclipse.jface.fieldassist.TextContentAdapter;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class SamplePart {
 
-	private Label label;
-	private TableViewer tableViewer;
 
 	@PostConstruct
 	public void createComposite(Composite parent) {
-		parent.setLayout(new GridLayout());
+		GridLayout layout = new GridLayout(2, false);
+		// parent is a Composite
+		parent.setLayout(layout);
+		Label lblPleaseEnterA = new Label(parent, SWT.NONE);
+		lblPleaseEnterA.setText("Please enter a value:");
+		
+		Text text = new Text(parent, SWT.BORDER);
+		GridData gd_text = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		gd_text.horizontalIndent = 8;
+		text.setLayoutData(gd_text);
+		GridData data = new GridData(SWT.FILL, SWT.TOP, true, false);
+		text.setData(data);
 
-		label = new Label(parent, SWT.NONE);
-		label.setText("Sample table");
+		// create the decoration for the text component
+		final ControlDecoration deco = new ControlDecoration(text, SWT.TOP
+			| SWT.LEFT);
 
-		tableViewer = new TableViewer(parent);
-		tableViewer.add("Sample item 1");
-		tableViewer.add("Sample item 2");
-		tableViewer.add("Sample item 3");
-		tableViewer.add("Sample item 4");
-		tableViewer.add("Sample item 5");
-		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		// use an existing image
+		Image image = FieldDecorationRegistry.getDefault()
+			.getFieldDecoration(FieldDecorationRegistry.DEC_INFORMATION)
+			.getImage();
+
+		// set description and image
+		deco.setDescriptionText("Use CNTL + SPACE to see possible values");
+		deco.setImage(image);
+
+		// always show decoration
+		deco.setShowOnlyOnFocus(false);
+
+		// hide the decoration if the text componet has content
+		text.addModifyListener(new ModifyListener() {
+			@Override
+			public void modifyText(ModifyEvent e) {
+				Text text = (Text) e.getSource();
+				if (text.getText().length() > 0) {
+				  deco.hide();
+				} else {
+				  deco.show();
+				}
+			}
+		});
+
+		// help the user with the possible inputs
+		// "." and "#" activate the content proposals
+		char[] autoActivationCharacters = new char[] { '.', '#' };
+		KeyStroke keyStroke;
+		//
+		try {
+			keyStroke = KeyStroke.getInstance("Ctrl+Space");
+			ContentProposalAdapter adapter = new ContentProposalAdapter(text,
+				new TextContentAdapter(),
+				new SimpleContentProposalProvider(new String[] {
+					"ProposalOne", "ProposalTwo", "ProposalThree" }),
+					keyStroke, autoActivationCharacters);
+			} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
+
 	}
 
-	@Focus
-	public void setFocus() {
-		tableViewer.getTable().setFocus();
-	}
+	
 }
